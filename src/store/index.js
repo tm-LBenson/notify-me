26045366008bc99f2d6d219a4875b57a2cf3e6f3
tm-LBenson <tm.lewisbenson@gmail.com>
2023-05-16 15:19:13 -0400

    add slice for selected tasks and students

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import loggerMiddleware from './middleware/logger';
 import projects from './slices/projects';
 import selectedProject from './slices/selectedProject';
+import taskSlice from './slices/tasks/tasksSlice';
+import selectedSlice from './slices/tasks/selectedSlice';
+import studentSlice from './slices/tasks/studentSlice';
 const store = configureStore({
   reducer: {
     projects,
     selectedProject,
+    tasks: taskSlice,
+    selected: selectedSlice,
+    students: studentSlice,
   },
 
   middleware: [loggerMiddleware],
