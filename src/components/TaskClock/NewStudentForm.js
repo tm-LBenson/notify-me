bd862b5f392f2700905093bbe103c460405f1412
tm-LBenson <tm.lewisbenson@gmail.com>
2023-05-16 14:37:39 -0400

    create component for adding new students

---
+++ src/components/TaskClock/NewStudentForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addStudent } from '@src/store/slices/tasks/studentSlice';
+import { setSelectedStudent } from '@src/store/slices/tasks/selectedSlice';
+
+const NewStudentForm = () => {
+  const [studentName, setStudentName] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (studentName.trim() !== '') {
+      const newStudent = {
+        name: studentName,
+      };
+      dispatch(addStudent(newStudent));
+      dispatch(setSelectedStudent(newStudent));
+      setStudentName('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={studentName}
+        onChange={(e) => setStudentName(e.target.value)}
+        placeholder="Enter student's name"
+        required
+      />
+      <button type="submit">Add New Student</button>
+    </form>
+  );
+};
+
+export default NewStudentForm;
