b4d56e6a0b4b171c19f0439b94dafc1dcff5fb14
tm-LBenson <tm.lewisbenson@gmail.com>
2023-05-28 11:33:34 -0400

    create actions for mentor sessions

--- src/store/slices/classes/studentSlice.js
+++ src/store/slices/classes/studentSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   students: [],
+  mentorSessions: [],
 };
 
 const studentsSlice = createSlice({
@@ -11,9 +12,23 @@ const studentsSlice = createSlice({
     addStudent: (state, action) => {
       state.students.push(action.payload);
     },
+    getAllStudents: (state, action) => {
+      state.students = action.payload;
+    },
+    addMentoringSession: (state, action) => {
+      state.mentorSessions.push(action.payload);
+    },
+    getAllMentoringSession: (state, action) => {
+      state.mentorSessions = action.payload;
+    },
   },
 });
 
-export const { addStudent } = studentsSlice.actions;
+export const {
+  addStudent,
+  getAllStudents,
+  addMentoringSession,
+  getAllMentoringSession,
+} = studentsSlice.actions;
 
 export default studentsSlice.reducer;
